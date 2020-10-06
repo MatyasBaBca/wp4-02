329b14581e9e94e3bba0b3e3fc5ac8cbb98353a9
MatyasBaBca <47145006+MatyasBaBca@users.noreply.github.com>
2020-10-06 14:28:15 +0200

    Add files via upload

---
+++ computer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="cz">
+<head>
+    <title>Document</title>
+</head>
+<body>
+<?php
+class Computer
+{
+    public string $GPU;
+    public string $CPU;
+    public string $RAM;
+    public string $SSD;
+    public string $case;
+    public string $MB;
+    public string $mouse;
+    public int $power;
+
+    public function __construct() {
+        
+}
+
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'classes/computer.php';
+?>
+
+<!DOCTYPE html>
+<html lang="cz">
+<head>
+    <title>Document</title>
+</head>
+<body>
+<?php
+$pc1 = new Computer ;
+$pc1->CPU = 'i5 3,6 GHz';
+$pc1->GPU = 'Nvidia 3080 GTX';
+$pc1->RAM = 'HyperX 32 GB';
+$pc1->SSD = 'Kingston A400 500GB';
+$pc1->case = 'Zalman S8';
+$pc1->MB = 'ASUS ROG Strix';
+$pc1->mouse = 'A4tech Bloody v8';
+
+$pc2 = new Computer ;
+$pc2->CPU = 'i7 3,6 GHz';
+$pc2->GPU = 'Nvidia GeForce GTX 1060';
+$pc2->RAM = 'HyperX 16 GB';
+$pc2->SSD = 'Kingston A400 250GB';
+$pc2->case = 'Zalman S3';
+$pc2->MB = 'Strix ASUS GT23 ';
+$pc2->mouse = 'A2tech Bloody v4';3
+var_dump($pc1);
+var_dump($pc2);
+
+
+
+?>
+
+
+
+</body>
+</html>
